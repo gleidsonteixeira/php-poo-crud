77b7cd1ce9862cd951ec9a2f760756ca4f399aec
angelolustosa <>
2022-11-16 13:00:49 -0300

    crud php

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$rota = $_SERVER['REQUEST_URI'];
+
+//usando require_once pq esse arquivo é importante, e nao pode jamais ser duplicado
+require_once '../src/controller/alunoController.php';
+require_once '../src/connection/conexao.php';
+
+$paginas = [
+    '/' => 'inicio',
+    '/listar' => 'listar',
+    '/novo' => 'novo',
+    '/editar' => 'editar'
+];
+
+include '../src/views/menu.phtml';
+
+if (false === isset($paginas[$rota])) {
+    include '../src/views/erro404.phtml';
+    exit;
+}
+
+echo $paginas[$rota](); // inicio()
